3bb0f7387498db45cc67d91e618433bedb03b225
pawik077 <pawik.r.s.077@gmail.com>
2025-02-12 12:23:03 +0100

    Removed misleading text

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut game = Game::new();
     loop {
         game.display_board();
-        print!("Enter move (e.g., e2e4): ");
+        print!("Enter move: ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
